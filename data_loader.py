fe18735e70fc04be5a882b961e6d787782b01bc1
kevinbro96 <kwyang@mail.ustc.edu.cn>
2021-08-08 17:50:39 +0800

    delta analysis

--- data_loader.py
+++ data_loader.py
@@ -9,6 +9,34 @@ from robustness.tools import folder, constants
 from robustness.tools.helpers import get_label_mapping
 from robustness.data_augmentation import Lighting
 
+ranges=[151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173
+    , 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196,
+       197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219,
+       220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242,
+       243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265,
+       266, 267, 268,281, 282, 283, 284, 285,32, 30, 31,33, 34, 35, 36, 37,80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+       91, 92, 93, 94, 95, 96, 97, 98, 99, 100,365, 366, 367, 368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 378, 379,
+       380, 381, 382,389, 390, 391, 392, 393, 394, 395, 396, 397,120, 121, 118, 119,300, 301, 302, 303, 304, 305, 306,
+       307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319]
+
+def subclass_label_mapping(classes, class_to_idx, ranges):
+    # add wildcard
+    # range_sets.append(set(range(0, 1002)))
+    mapping = {}
+    for class_name, idx in class_to_idx.items():
+        for new_idx, range_set in enumerate(ranges):
+            if idx == range_set:
+                mapping[class_name] = new_idx
+        # assert class_name in mapping
+    filtered_classes = list(mapping.keys()).sort()
+    return filtered_classes, mapping
+
+def get_subclass_label_mapping(ranges):
+    def label_mapping(classes, class_to_idx):
+        return subclass_label_mapping(classes, class_to_idx, ranges=ranges)
+    return label_mapping
+
+
 def getSVHN(batch_size, TF, data_root='/tmp/public_dataset/pytorch', train=True, val=True, **kwargs):
     data_root = os.path.expanduser(os.path.join(data_root, 'svhn-data'))
     num_workers = kwargs.setdefault('num_workers', 1)
@@ -83,16 +111,15 @@ def getCIFAR100(batch_size, TF, data_root='/tmp/public_dataset/pytorch', train=T
             batch_size=batch_size, shuffle=False, **kwargs)
         ds.append(test_loader)
     ds = ds[0] if len(ds) == 1 else ds
-    return ds
+    return dst
 
 def getIMAGENET(batch_size, TF, data_root='/tmp/public_dataset/pytorch', train=True, val=True, **kwargs):
-    traindir = os.path.join(data_root, 'ILSVRC2012_img_train')
-    valdir = os.path.join(data_root, 'ILSVRC2012_img_val')
+    traindir = os.path.join(data_root, 'train')
+    valdir = os.path.join(data_root, 'val')
     num_workers = kwargs.setdefault('num_workers', 1)
     kwargs.pop('input_size', None)
     ds = []
-    label_map = get_label_mapping('restricted_imagenet',
-                                  constants.RESTRICTED_IMAGNET_RANGES)
+    label_map = get_subclass_label_mapping(ranges)
     if train:
         train_dataset = folder.ImageFolder(root=traindir,
                                            transform=TF,
